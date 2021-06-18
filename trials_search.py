98360808fd3c98801f56ff8e9b2b7993b47c9916
Rossi_py <rossi88@gmail.com>
2021-06-18 18:59:12 -0300

    small changes

--- trials_search.py
+++ trials_search.py
@@ -9,8 +9,18 @@ def Add_file(files):
         return files
     return
 
-def Load_trials():
-    files=[]
+def Load_trials(files=[], trials=[]):
+    """
+    Takes in files (list of strings with names of the files to load)
+    Takes in a trials list (of previously instanced trials)
+    if it empty, it will request user to input each file
+    if trials empty it will start from scratch
+    Reads files and instances one Trial for each trial
+    the files must have 7 text fields in the order
+    title, status, study_results, conditions, interventions, locations, url
+    where the first line is a titles line that gets ignored.
+    Returns list trials with references to each trial instanced
+    """
 
     # adds each file to files list
     while True:
@@ -32,11 +42,17 @@ def Load_trials():
                 #splits by tabs
                 fields=line.split("\t")
                 # instances a new trial for each line and includes in list of trials
-                trials.append(Trial(fields[0], fields[1], fields[2], fields[3], fields[4],fields[5], fields[6]))
+                trials.append(Trial(fields[0], fields[1], fields[2], fields[3], fields[4],fields[5], fields[6], file))
+    return trials
 
 class Trial(object):
+    """
+    Class that holds an instance for each trial
+    """
     id_counter=0
-    def __init__(self, title, status, study_results, conditions, interventions, locations, url):
+    def __init__(self, title, status, study_results, conditions, interventions, locations, url, file_name):
+        Trial.id_counter += 1
+        self.id=Trial.id_counter
         self.title=title
         self.status=status
         self.study_results=study_results
@@ -44,10 +60,10 @@ class Trial(object):
         self.interventions=interventions
         self.locations=locations
         self.url=url
-        Trial.id_counter += 1
-        self.id=Trial.id_counter
+        self.file_name = file_name
 
 
 if __name__ == '__main__':
-    trials=[]
+    # laods trials asking the user for file names and starts trials from an empty list
     Load_trials()
+
